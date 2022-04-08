82e3686069f432f0b0bf1c7277705cca3ae99b82
julie <perianmodely.julie@gmail.com>
2022-04-08 09:00:57 +0400

    first commit

---
+++ database/migrations/2022_04_07_104710_create_topics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('poster');
+            $table->string('genre');
+            $table->string('date');
+            $table->string('realisateur');
+            $table->string('acteur');
+            $table->text('synopsis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
